201b270e5d1634f6b14389a6ccc2596dbb135e95
borisbrunop <borisbruno88@gmail.com>
2020-08-14 17:21:07 -0400

    se hizo el creado de la agenda y del contacto falta mostrar los contactos, la funcion delete y la funcion edit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,61 @@
-const getState = ({ getStore, setStore }) => {
+const baseUrl = "https://assets.breatheco.de/apis/fake/contact/";
+const agenda = "Agenda-Boris";
+const getState = ({ getStore, setStore, getActions }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
+			contacts: []
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+			fetchCreateContact: async (name, email, phone, adress) => {
+				try {
+					let response = await fetch(`${baseUrl}`, {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/JSON"
+						},
+						body: JSON.stringify({
+							full_name: name,
+							email,
+							agenda_slug: agenda,
+							address: adress,
+							phone
+						})
+					});
+					if (response.ok) {
+						contacts = await response.json();
+					} else {
+						console.log(`error: ${response.status} ${response.statusText}`);
+					}
+				} catch (error) {
+					console.log("something failed");
+					console.log(error);
+				}
+			},
+			fetchContacts: async () => {
+				let contacts = [];
+				try {
+					let response = await fetch(`${baseUrl}/agenda/${agenda}`, {
+						method: "GET",
+						headers: {
+							"Content-Type": "application/JSON"
+						}
+					});
+					if (response.ok) {
+						contacts = await response.json();
+					} else {
+						console.log(`error: ${response.status} ${response.statusText}`);
+					}
+				} catch (error) {
+					console.log("something failed");
+					console.log(error);
+				}
+				setStore({
+					contacts: contacts
+				});
+			}
 		}
 	};
 };
--- src/js/views/Contacts.js
+++ src/js/views/Contacts.js
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-
+import { Context } from "../store/appContext";
 import { ContactCard } from "../component/ContactCard.js";
 import { Modal } from "../component/Modal";
 
 export const Contacts = () => {
+	const { store, actions } = useContext(Context);
 	const [state, setState] = useState({
 		showModal: false
 	});
+
+	useEffect(() => {
+		actions.fetchContacts();
+	}, []);
 
 	return (
 		<div className="container">
@@ -19,10 +24,10 @@ export const Contacts = () => {
 				</p>
 				<div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
 					<ul className="list-group pull-down" id="contact-list">
-						<ContactCard onDelete={() => setState({ showModal: true })} />
-						<ContactCard />
-						<ContactCard />
-						<ContactCard />
+						{store.contacts.map(contact => {
+							console.log(contact);
+							<ContactCard contact={contact} onDelete={() => setState({ showModal: true })} />;
+						})}
 					</ul>
 				</div>
 			</div>
